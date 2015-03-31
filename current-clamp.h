61b4ac95938c8259c176bbc6a2b5daba572bd8c1
Ansel <aesthapus@gmail.com>
2015-03-31 14:56:03 -0400

    changed indentation format for file

--- current-clamp.h
+++ current-clamp.h
@@ -24,93 +24,91 @@
 
 class Clamp : public DefaultGUIModel {
 
-Q_OBJECT
+	Q_OBJECT
 
-public:
-  Clamp(void);
-  virtual ~Clamp(void);
+	public:
+		Clamp(void);
+		virtual ~Clamp(void);
 
-  void execute(void);
-//	void createGUI(DefaultGUIModel::variable_t *, int);
-  void customizeGUI(void);
+		void execute(void);
+		void customizeGUI(void);
 
-  //Custom flags for clamp mode
-  enum mode_t {
-    RAMP, STEP,
-  };
+		//Custom flags for clamp mode
+		enum mode_t {
+			RAMP, STEP,
+		};
 
-public slots:
+	public slots:
 
-  signals: // custom signals
+	signals: // custom signals
+		void setFIRange(double newminamp, double newmaxamp, double minHz, double maxHz);
+		void newDataPoint(double newx, double newy);
+		void setStepMode(bool);
+		void setPlotMode(bool);
+		void saveImage(QString fileName);
+		void drawFit(double* x, double* y, int size);
+		void setEqnMsg(const QString &);
 
-  void setFIRange(double newminamp, double newmaxamp, double minHz, double maxHz);
-  void newDataPoint(double newx, double newy);
-  void setStepMode(bool);
-  void setPlotMode(bool);
-  void saveImage(QString fileName);
-  void drawFit(double* x, double* y, int size);
-  void setEqnMsg(const QString &);
+	protected:
+		virtual void update(DefaultGUIModel::update_flags_t);
 
-protected:
-  virtual void update(DefaultGUIModel::update_flags_t);
+	private:
+		// inputs, states, calculated values
+		double systime;
+		double Vm;
+		double dt;
+		// parameters
+		double minamp;
+		double maxamp;
+		double stepsize;
+		double delay;
+		double width;
+		double repeat;
+		// options
+		bool randomize;
+		bool plotFI;
+		mode_t mode;
+		// bookkeeping
+		double spktime;
+		double prevspktime;
+		double newminamp;
+		double newmaxamp;
+		int stepcount;
+		int cyclecount;
+		int nstep;
+		double delay0;
+		bool random;
+		long long count;
+		int spikecount;
+		double spikestate;
+		RunningStat ISI;
 
-private:
-  // inputs, states, calculated values
-  double systime;
-  double Vm;
-  double dt;
-  // parameters
-  double minamp;
-  double maxamp;
-  double stepsize;
-  double delay;
-  double width;
-  double repeat;
-  // options
-  bool randomize;
-  bool plotFI;
-  mode_t mode;
-  // bookkeeping
-  double spktime;
-  double prevspktime;
-  double newminamp;
-  double newmaxamp;
-  int stepcount;
-  int cyclecount;
-  int nstep;
-  double delay0;
-  bool random;
-  long long count;
-  int spikecount;
-  double spikestate;
-  RunningStat ISI;
+		double* arrstep;
+		double* arrrate;
+		double* arrFIamp; // internally store FI data points
+		double* arrFIHz;
+		double yrangemin;
+		double yrangemax;
+		// QT components
+		QString eqnmsg;
+		ScatterPlot *splot;
 
-  double* arrstep;
-  double* arrrate;
-  double* arrFIamp; // internally store FI data points
-  double* arrFIHz;
-  double yrangemin;
-  double yrangemax;
-  // QT components
-  QString eqnmsg;
-  ScatterPlot *splot;
+		// Clamp functions
+		void initParameters();
+		void initStepArray();
+		void bookkeep();
+		void countspikes();
+		bool OpenFile(QString);
+		QFile dataFile;
+		QDataStream stream;
 
-  // Clamp functions
-  void initParameters();
-  void initStepArray();
-  void bookkeep();
-  void countspikes();
-  bool OpenFile(QString);
-  QFile dataFile;
-  QDataStream stream;
-
-private slots:
-  void togglerandom(bool);
-  void updateClampMode(int);
-  void toggleFIplot(bool);
-  void saveFIData();
-  void fitData();
-  void print();
-  void exportSVG();
-  void clearData();
+	private slots:
+		void togglerandom(bool);
+		void updateClampMode(int);
+		void toggleFIplot(bool);
+		void saveFIData();
+		void fitData();
+		void print();
+		void exportSVG();
+		void clearData();
 };
